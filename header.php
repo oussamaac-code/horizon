6fd58ec3ffe7ffc17e0952806fea2587a94ad0d5
oussama ait chaoui <oussamaaitchaoui@MacBook-Air-oussama.local>
2023-09-11 23:22:39 +0100

    edit header title

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title> <?php echo get_the_title() ;?> - shopify</title>
 
     <?php wp_head() ;?>
 </head>
@@ -20,7 +20,7 @@
         <div class="logo"> <!-- logo -->
 
             <a href="<?php echo get_home_url() ;?>">
-            
+
                 <img src="<?php ;?>" alt="logo">
             </a>
 

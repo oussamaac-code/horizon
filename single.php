be1b80baea840ef88a31d9a37f4b399b8e26f5e5
oussama ait chaoui <oussamaaitchaoui@MacBook-Air-oussama.local>
2023-09-18 23:36:19 +0100

    add article details to the page

--- single.php
+++ single.php
@@ -6,7 +6,16 @@
 
         <section class="blog">
             <div class="container">
-                <div class="article"></div>
+                <div class="article">
+                    <div class="thumbnail">
+                        <?php echo get_the_post_thumbnail() ;?>
+                    </div>
+                    <div class="meta"></div>
+                    <article>
+                        <?php the_content();?>
+                    </article>
+                </div>
+                
                 <div class="sidebar">
                     <?php if(is_active_sidebar('blog-sidebar')): ?>
 

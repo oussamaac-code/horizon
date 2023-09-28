c852207786559fc574e6b67711bb61fd44c5e238
oussama ait chaoui <oussamaaitchaoui@MacBook-Air-oussama.local>
2023-09-28 12:21:45 +0100

    gulpfile sass

--- gulpfile.js
+++ gulpfile.js
@@ -17,19 +17,31 @@ function style(){
 
 }
 
+function style_admin(){
+
+    return gulp.src('./core/admin/sass/**/*.scss')
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(concat('edit.css'))
+    .pipe(gulp.dest('./core/admin/css'))
+    .pipe(browserSync.stream())
+
+}
+
 
 function watch(){
 
     browserSync.init({
-        proxy: 'http://shopify.local/',
+        proxy: 'http://horizon.local/',
 
       })
 
     gulp.watch('./sass/**/*.scss', style)
+    gulp.watch('./core/admin/sass/**/*.scss', style_admin)
     gulp.watch('./**/*.php').on('change', browserSync.reload)
   
 }
 
 
 exports.style = style;
+exports.style_admin = style_admin;
 exports.watch = watch;

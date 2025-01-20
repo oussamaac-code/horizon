98932e4e3ac7cf6ea22594c064ca63edeb378d54
Oussamaac <oussamaac.dev@gmail.com>
2025-01-20 18:46:58 +0100

    blog update

--- inc/ajax.php
+++ inc/ajax.php
@@ -24,3 +24,25 @@ function my_ajax_handler_featured_product() {
     echo do_shortcode('[products columns="4" category="'.$_POST['category'].'" limit="8" ]');
 	wp_die();
 }
+
+/**
+ * Handles AJAX update quantity cart
+ */
+
+ add_action( 'wp_footer', function() {
+	
+	?><script>
+	jQuery( function( $ ) {
+		let timeout;
+		$('.woocommerce').on('change', 'input.qty', function(){
+			if ( timeout !== undefined ) {
+				clearTimeout( timeout );
+			}
+			timeout = setTimeout(function() {
+				$("[name='update_cart']").trigger("click");
+			}, 800 );
+		});
+	} );
+	</script><?php
+	
+} );

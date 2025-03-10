060a537d51bfac63a8c49073e2afa5dac7d8dbf8
Oussamaac <oussamaac.dev@gmail.com>
2025-03-10 21:04:31 +0000

    siteloader

--- templates/parts/site-loader.php
+++ templates/parts/site-loader.php
@@ -1,6 +1,40 @@
-<div class="site-loader">
+<?php 
+
+$logo_white = get_theme_mod('horizon_header_logo');
+$logo_dark  = get_theme_mod('horizon_header_logo_dark');
+
+;?>
+<div class="site-loader loader-show dark">
     <div class="loader-icon">
-        <img src="<?php echo get_option('site_logo') ;?>" alt="loader">
+        <!-- dark -->
+        <?php echo wp_get_attachment_image($logo_dark, 'medium') ;?> 
+        <!-- white -->
+        <?php echo wp_get_attachment_image($logo_white, 'medium') ;?> 
     </div>
     <div class="loader-bg"></div>
 </div>
+
+<script>
+    jQuery(document).ready(function($){
+
+        $('.site-loader').removeClass('loader-show')
+
+        $(document).on("click", "a", function (e) {
+            e.preventDefault();
+        
+            var linkClicked = $(this).attr('href');
+
+            if ( linkClicked!='#' && linkClicked!=undefined && linkClicked!=""){
+
+                $('.site-loader').addClass('loader-show')
+                window.setTimeout(function () {
+                    
+                    window.location = linkClicked;
+                }, 1500);
+            
+                return false;
+            }
+        });
+
+    })
+</script>
